de1be30440e6f1b6eb4f6f376bdb88303339dfe1
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2022-09-16 14:12:20 -0500

    .

--- func2.py
+++ func2.py
@@ -2,9 +2,18 @@ def slope(x1, y1, x2, y2):
     s = (y2 - y1)/(x2 - x1)
     return s 
 
+def yintercept(x1, x2, y1, y2):
+    yintercept = -(slope(x1,y1,x2,y2)*x1) + y1 
+    return yintercept 
+
 x1 = 2
-y1 = 5
+y1 = 10
 x2 = 4
 y2 = 10
 
-print(slope(x1,y1,x2,y2))
+if slope(x1,y1,x2,y2) == 0:
+    print("Slope = None")
+else:
+    print("Slope = ", slope(x1,y1,x2,y2))
+    
+print("y-intercept = ", yintercept(x1,x2,y1,y2))
