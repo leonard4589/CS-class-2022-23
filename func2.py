30e006f816617561cc83a9000674b34295cd19c3
leonard4589 <75444828+leonard4589@users.noreply.github.com>
2022-09-16 14:30:45 -0500

    .

--- func2.py
+++ func2.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.plyplot as plt 
+
+
 def slope(x1, y1, x2, y2):
     try:
         s = (y2 - y1)/(x2 - x1)
@@ -12,11 +16,27 @@ def yintercept(x1, x2, y1, y2):
         yintercept = None
     return yintercept 
 
+def graph(x1, x2, y1, y2):
+    y = slope(x1, x2, y1, y2)*x1 + yintercept(x1, x2, y1, y2)
+    return y 
+
 x1 = 2
 y1 = 5
 x2 = 2
 y2 = 10
 
+x = []
+y = []
+
+for i in range(-10,10):
+    x.append(i)
+    y.append(f(i))
+
 print("Slope = ", slope(x1,y1,x2,y2))
 
 print("y-intercept = ", yintercept(x1,x2,y1,y2))
+
+fig, ax = plt.subplots()
+ax.plot(xa, ya)
+
+plt.show()
